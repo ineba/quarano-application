b44bc05024bf884e0610581551f4470f022867e0
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-23 15:50:09 +0100

    CORE-676 e2e/s7 implemented until step 12

---
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s7.spec.ts
@@ -0,0 +1,104 @@
+/// <reference types="cypress" />
+/* 
+0 - Login als Gama "agent1"
+1 - wähle Übersichtsseite "Indexfälle"
+2 - wähle "neuen Indexfall anlegen"
+3 - Vorname -> "Berta"
+4 - Nachname ->  "Benz"
+5 - Geburtsdatum -> "25.03.1946"
+6 - Telefonnummer -> "062186319"
+7 - Email -> "bbenz@mail.de"
+8 - Straße -> "Waldweg"
+9 - Hausnummer -> "2"
+10 - PLZ von Mannheim -> "68167"
+11 - Stadt -> "Mannheim"
+12 - wähle "Speichern und schließen
+--> "CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "angelegt"
+13 - wähle Indexfall "Berta Benz" aus
+14 - wähle "Nachverfolgung Starten"
+15 - Extrahiere Anmeldelink aus dem Template
+--> CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "in Registrierung"
+16 - Logout als GAMA
+17 - Anmeldelink aufrufen
+18 - Klick auf Weiter
+19 - Benutzername: "Berta"
+20 - Passwort: "Password03!"
+21 - Passwort bestätgen  "Password03!"
+22 - Geburtsdatum: "25.03.1946"
+23 - AGB aktivieren
+24 - Klick auf "Registrieren" Button
+25 - Klick auf "weiter"
+26 - Logout als Bürger
+27 -  Login als Gama "agent1"CHECK:  In Übersicht "Indexfälle" steht für "Berta Benz" der Status "Registrierung abgeschlossen"
+28 - Logout als GAMA
+29 - Login als Bürger ("Berta"; "Password03!")
+30 - Initialer Fragebogen "Covid-19-Symptome" -> "nein"
+31 - Bitte geben Sie Ihren behandelnden Hausarzt an. -> Dr. Schmidt
+32 - Nennen Sie uns bitte den (vermuteten) Ort der Ansteckung: -> "Familie"
+33 - Haben Sie eine oder mehrere relevante Vorerkrankungen? -> "nein"
+34 - Arbeiten Sie im medizinischen Umfeld oder in der Pflege? -> "nein"
+35 - Haben Sie Kontakt zu Risikopersonen? -> "nein"
+36 - Klick "weiter"
+37 - Kontakte mit anderen Menschen -> "Carl Benz"
+38 - Klick enter 
+39 - wähle "Kontakt anlegen" in Popup
+40 - Telefonnummer (mobil) -> "017196347526"
+41 - Klick auf "speichern"
+42 - Klick auf "Erfassung abschließen"
+43 - Logout als Bürger
+44 - Login als GAMA "agent1"
+--> CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "In Nachverfolgung"
+45 - suche Indexfall "Berta Benz"
+46 - wähle "Fall abschließen"
+47 - Popup "Diesen Fall abschließen" geht auf
+48 - Zusätzliche Informationen zum Fallabschluss: -> "Quarantäne beendet"
+49 - Klicke "OK"
+50 - wähle in Übersicht der Indexfälle den Filter "abgeschlossen"
+--> CHECK: In Übersicht "Indexfälle" steht für "Berta Benz" der Status "abgeschlossen"
+51 - Logout als GAMA
+*/
+
+describe('S7 - Status wechselt korrekt', () => {
+  it('should run', () => {
+    /* 0 - Login als Gama "agent1" */
+    cy.loginAgent();
+    //cy.wait(100);
+
+    /* 1 - wähle Übersichtsseite "Indexfälle" */
+    cy.get('[data-cy="index-cases"]').should('exist').click();
+
+    /* 2 - wähle "neuen Indexfall anlegen" */
+    cy.get('[data-cy="new-case-button"]').should('exist').click();
+
+    /* 3 - Vorname -> "Berta" */
+    cy.get('[data-cy="input-firstname"]').should('exist').type('Berta');
+
+    /* 4 - Nachname ->  "Benz" */
+    cy.get('[data-cy="input-lastname"]').should('exist').type('Benz');
+
+    /* 5 - Geburtsdatum -> "25.03.1946" */
+    cy.get('[data-cy="input-dayofbirth"]').should('exist').type('25.03.1946');
+
+    /* 6 - Telefonnummer -> "062186319" */
+    cy.get('[data-cy="input-phone"]').should('exist').type('062186319');
+
+    /* 7 - Email -> "bbenz@mail.de" */
+    cy.get('[data-cy="input-email"]').should('exist').type('bbenz@mail.de');
+
+    /* 8 - Straße -> "Waldweg" */
+    cy.get('[data-cy="street-input"]').should('exist').type('Waldweg');
+
+    /* 9 - Hausnummer -> "2" */
+    cy.get('[data-cy="house-number-input"]').should('exist').type('2');
+
+    /* 10 - PLZ von Mannheim -> "68167" */
+    cy.get('[data-cy="zip-code-input"]').should('exist').type('68167');
+
+    /* 11 - Stadt -> "Mannheim" */
+    cy.get('[data-cy="city-input"]').should('exist').type('Mannheim');
+
+    /* 12 - wähle "Speichern und schließen" */
+    cy.get('[data-cy="client-submit-and-close-button"] button').click();
+    cy.route('POST', '/hd/cases/?type=index').as('newIndex');
+  });
+});
